0c4e0463c2d7502e4917a82f5b9a28cec17fdde3
Hitahci <stefan36rus@gmail.com>
2022-11-24 20:33:45 +0300

    1

---
+++ Home1/task3.py
@@ -0,0 +1,13 @@
+#3) Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. 
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+num = int(input("Введите число от 0 до 9: "))
+if num == 0:
+    result = 0
+elif num > 9:
+    result = "Вы ввели число больше 9"
+else:
+    num_num = num * 10 + num
+    num_num_num = num_num * 10 + num
+    result = num + num_num + num_num_num
+print(result)
