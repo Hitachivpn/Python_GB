14b46ad657459dda4ae2a86ffa39f62a5739d50d
Hitahci <stefan36rus@gmail.com>
2022-12-13 21:28:14 +0300

    Home 4 rework

--- Home4/task4.py
+++ Home4/task4.py
@@ -1,41 +1,11 @@
-# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
-# Пример:
-# k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+#4) Представлен список чисел. Определить элементы списка, не имеющие повторений. 
+# Сформировать итоговый массив чисел, соответствующих требованию. Элементы вывести 
+# в порядке их следования в исходном списке. Для выполнения задания обязательно использовать генератор.
+#Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
+#Результат: [23, 1, 3, 10, 4, 11]
 
-import random
 
-def write_file(st):
-    with open('task4.txt', 'w') as data:
-        data.write(st)
-
-def rnd():
-    return random.randint(0,101)
-
-def create_mn(k):
-    lst = [rnd() for i in range(k+1)]
-    return lst
-    
-def create_str(sp):
-    lst= sp[::-1]
-    wr = ''
-    if len(lst) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(lst)):
-            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-                wr += f'{lst[i]}x^{len(lst)-i-1}'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 2 and lst[i] != 0:
-                wr += f'{lst[i]}x'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 1 and lst[i] != 0:
-                wr += f'{lst[i]} = 0'
-            elif i == len(lst) - 1 and lst[i] == 0:
-                wr += ' = 0'
-    return wr
-
-k = int(input("Введите натуральную степень k = "))
-koef = create_mn(k)
-write_file(create_str(koef))
+my_list = [4, 7, 6, 4, 9, 1, 7, 1, 5, 3]
+print("Исходные элементы списка:\n", my_list)
+new_list = [i for i in my_list if my_list.count(i) == 1]
+print("Элементы списка, не имеющие повторений:\n", new_list)
